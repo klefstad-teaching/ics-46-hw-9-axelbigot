9fa663d8c8ce5f349bc8c55ecd70a16b089d9f83
Axel Bigot <abigot@uci.edu>
2025-03-14 23:33:33 -0700

    chore: fix typo

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -64,5 +64,5 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 void print_path(const vector<int>& v, int total) {
 	for (int i : v) cout << i << " ";
 	cout << endl;
-	cout << "Total Cost is " << total << endl;
+	cout << "Total cost is " << total << endl;
 }
